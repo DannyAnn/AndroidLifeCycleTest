0cbbaa5bf05883cfbb2d65b27c162f2db945278b
Danny <dannyann95@foxmail.com>
2017-04-21 15:48:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidlifecycletest/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.androidlifecycletest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+
+        if(savedInstanceState != null)
+        {
+            String data = savedInstanceState.getString("tempData");
+            Log.d(TAG,data);
+        }
+        setContentView(R.layout.activity_main);
+
+        Button startNormalButton = (Button)findViewById(R.id.start_normal_activity);
+        Button startDialogButton = (Button)findViewById(R.id.start_dialog_activity);
+
+        startNormalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NormalAcitivity.class);
+                startActivity(intent);
+            }
+        });
+
+        startDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "Something you just typed";
+        outState.putString("data_key",tempData);
+    }
+}
---
+++ app/src/main/java/com/example/androidlifecycletest/NormalAcitivity.java
@@ -0,0 +1,13 @@
+package com.example.androidlifecycletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NormalAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.normal_layout);
+    }
+}
